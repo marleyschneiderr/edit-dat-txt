845c30f021f6544b09340cffe215b475894402b7
marleyschneiderr <marley33@uw.edu>
2023-05-14 17:16:42 -0700

    Fixing database file and adding logic for PUT and GET methods

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,45 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  
+  // database name and version
+  const editorDB = await openDB('jate', 1);
+
+  // creating a new transaction that specifies db and data privileges 
+  const tx = editorDB.transaction('jate', 'readwrite');
+
+  // open the correct object store
+  const store = tx.objectStore('jate');
+
+  // use add - .add() on the store and pass through the content 
+  const request = store.put({ id: 1, value: content });
+
+  // confirm correct
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  
+  // show the database and version 
+  const editorDB = await openDB('jate', 1);
+
+  // creating a new transaction that specifies db and data privileges 
+  const tx = editorDB.transaction('jate', 'readonly');
+
+  // open the correct object store
+  const store = tx.objectStore('jate');
+
+  // use all - .getAll() to get all data from the database
+  const request = store.getAll();
+
+  // get confirmation to see request
+  const result = await request;
+  console.log("🚀 - data read from database", result);
+  return result.value;
+};
 
 initdb();
